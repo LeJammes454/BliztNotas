bca38bde0d6758bfbaddfba15443c537704abb7d
Lejammes <s19120178@alumnos.itsur.edu.mx>
2022-11-03 19:41:36 -0600

    Creando la asplicacion de notas Yo la llame bliztNotas
    Implementando las dependencias y corrijiendo un error de kotlin-kapt

---
+++ app/src/main/java/com/lejammes454/bliztnotas/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.lejammes454.bliztnotas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
