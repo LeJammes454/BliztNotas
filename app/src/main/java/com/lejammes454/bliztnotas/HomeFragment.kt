518440de6b9fe3ef2c28861c7266da20a71b07c1
Lejammes <s19120178@alumnos.itsur.edu.mx>
2022-11-05 19:42:47 -0600

    Solucioando error de remplazar un fragmento por otro.. Se agreglo al gradle el puglin de 'kotlin-android-extensions' e importe kotlinx para el funcionamiento de un botonflotante

--- app/src/main/java/com/lejammes454/bliztnotas/HomeFragment.kt
+++ app/src/main/java/com/lejammes454/bliztnotas/HomeFragment.kt
@@ -1,13 +1,14 @@
 package com.lejammes454.bliztnotas
-
+import kotlinx.android.synthetic.main.fragment_home.view.*
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,14 +16,14 @@ class HomeFragment : Fragment() {
         }
     }
 
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-
-        return inflater.inflate(R.layout.fragment_home, container, false)
-
+        val n = inflater.inflate(R.layout.fragment_home, container, false)
+        return n
     }
 
     companion object {
@@ -30,8 +31,26 @@ class HomeFragment : Fragment() {
         fun newInstance() =
             HomeFragment().apply {
                 arguments = Bundle().apply {
-                    Log.i("JUAN","INGRESO AL FRAGMENTO JUTAN")
                 }
             }
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        fabBtnCreateNote.setOnClickListener {
+            remplazarFragmento(CreateNoteFragment.newInstance(),true)
+
+        }
+    }
+
+
+    fun remplazarFragmento(fragment:Fragment, istransition:Boolean){
+        val fragmentTransition = requireActivity().supportFragmentManager.beginTransaction()
+
+        if (istransition){
+            fragmentTransition.setCustomAnimations(android.R.anim.slide_out_right,android.R.anim.slide_in_left)
+        }
+        fragmentTransition.replace(R.id.frame_layout,fragment).addToBackStack(fragment.javaClass.simpleName).commit()
+    }
+
 }
